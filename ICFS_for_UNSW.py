b33183ac6ab80ef82775b9377ee0a0a268417a5b
MarcoLorenzoC <marcolorenzo.campo@mail.polimi.it>
2023-10-14 16:11:10 +0200

    updated ICFS

--- ICFS_for_UNSW.py
+++ ICFS_for_UNSW.py
@@ -1,47 +1,51 @@
 # UNSW-NB15 Computer Security Dataset: Analysis through Visualization
+import copy
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.feature_selection import mutual_info_classif
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 
+# Select the correlated features in the two datasets.
+def get_most_correlated_features(x1, x2, n=10):
+    # Convert all datasets to numbers
+    x1 = x1.apply(pd.to_numeric, errors='ignore')
+    x2 = x2.apply(pd.to_numeric, errors='ignore')
+
+    # Calculate the correlation matrix between the two DataFrames.
+    corr_matrix = x2.corrwith(x1)
+
+    # Select the features that are most correlated between the two DataFrames.
+    most_correlated_features = corr_matrix.abs().sort_values(ascending=False).index[:n]
+
+    # Return the most important features.
+    return most_correlated_features
+
+
+# compute all elements of df1 that are not in df2
+def compute_set_difference(df1, df2):
+    # Create a new DataFrame containing the set difference of the two DataFrames.
+    df_diff = df1[~df1.index.isin(df2.index)]
+    # Return the DataFrame.
+    return df_diff
+
+
 # Simulation parameters
 pd.set_option("display.max.columns", None)
 
+# division in layer based on attacks
+'''
 # Splitting the sets in two parts, to mirror the strategy used for the DLHA
 # First layer will be trained on 'attacks_for_l1', while second layer on 'attacks_for_l2'
 attacks_for_l1 = ['generic', 'exploits', 'fuzzers', 'dos']
 attacks_for_l2 = ['reconnaissance', 'analysis', 'backdoor', 'shellcode', 'worms']
+'''
 
-# import the dataset
-# df_test = pd.read_csv('UNSW-NB15_datasets/Partial Sets/UNSW_NB15_testing-set.csv', sep=',', header=0)
+# import the train set
 df_train = pd.read_csv('UNSW-NB15_datasets/Partial Sets/UNSW_NB15_training-set.csv', sep=',', header=0)
-
-# write the present features to file
-with open('UNSW-NB15 Outputs/Features_from_train_test.txt', 'w') as f:
-    for x in df_train.columns:
-        f.write(str(x) + '\n')
-
-# Do test and train differ in features?
-'''
-# let's see if they differ in features
-not_present_in_test = []
-for x in df_train.columns:
-    if x not in df_test.columns:
-        not_present_in_test.append(x)
-if len(not_present_in_test) == 0:
-    print("Test and train have the same features.")
-else:
-    print('Features in train not present in test: ' + str(not_present_in_test))
-'''
-
 # put everything in lowercase for train and test sets
 df_train = df_train.apply(lambda k: k.astype(str).str.lower())
-# df_test = df_test.apply(lambda k: k.astype(str).str.lower())
-
-# drop the ID column
-del df_train['id']
-# del df_test['id']
 
 # visualization part commented
 '''
@@ -80,59 +84,69 @@ plt.subplots_adjust(top=0.8, bottom=0.2)
 plt.savefig('UNSW-NB15 Outputs/attacks_plot_in_test-set.png', dpi=300)
 '''
 
-# The following is applied to train set and later to test set
+# write the present features to file
+'''
+with open('UNSW-NB15 Outputs/Features_from_train_test.txt', 'w') as f:
+    for x in df_train.columns:
+        f.write(str(x) + '\n')
+'''
+
+# Do test and train differ in features?
+'''
+# let's see if they differ in features
+not_present_in_test = []
+for x in df_train.columns:
+    if x not in df_test.columns:
+        not_present_in_test.append(x)
+if len(not_present_in_test) == 0:
+    print("Test and train have the same features.")
+else:
+    print('Features in train not present in test: ' + str(not_present_in_test))
+'''
+
+# drop the ID column
+del df_train['id']
+
+# Intersected Correlated Feature Selection (ICFS)
+
 # ignore the categorical values for now
-non_numerical = ['proto', 'service', 'state', 'attack_cat', 'label']
-to_encode = ['proto', 'service', 'state']
-num_col = list(set(df_train.columns) - set(non_numerical))
-to_append_train_atk = df_train['attack_cat']
-# to_append_test_atk = df_test['attack_cat']
-to_append_train_lbl = df_train['label']
-# to_append_test_lbl = df_test['label']
+categorical_features = ['proto', 'service', 'state', 'attack_cat']  # all non numerical features
+features_to_encode = ['proto', 'service', 'state']  # features to one-hot encode later
+numerical_features = list(set(df_train.columns) - set(categorical_features))
 
-# MinMax scales the values of df_train between 0-1
-scaler1 = MinMaxScaler()
+# now ICFS only on the numerical features
+num_trainset = copy.deepcopy(df_train)
+del num_trainset['proto']
+del num_trainset['service']
+del num_trainset['state']
 
-# scaling the numerical values of the train set
-df_minmax = scaler1.fit_transform(df_train[num_col])
-df_train[num_col] = pd.DataFrame(df_minmax, columns=df_train[num_col].columns)
-
-# Perform One-hot encoding of the categorical values
-ohe = OneHotEncoder(handle_unknown='ignore')
-label_enc = ohe.fit_transform(df_train[to_encode])
-label_enc.toarray()
-new_labels = ohe.get_feature_names_out(to_encode)
-df_enc = pd.DataFrame(data=label_enc.toarray(), columns=new_labels)
-df_train = pd.concat([df_train[num_col], df_enc, to_append_train_atk, to_append_train_lbl],
-                     axis=1)  # df_train includes the newly one-hot-encoded columns
-
-# sort the columns
-df_train = df_train.sort_index(axis=1)  # sort the columns
-# df_test = df_test.sort_index(axis=1)  # sort the columns
-
-# now let's perform Intersected Correlated Feature Selection
-generic = df_train[(df_train['attack_cat'] == 'generic')]
+# let's create a dataframe for each attack category, removing the 'attack_cat' feature
+generic = num_trainset[(num_trainset['attack_cat'] == 'generic')]
 del generic['attack_cat']
-exploits = df_train[(df_train['attack_cat'] == 'exploits')]
+exploits = num_trainset[(num_trainset['attack_cat'] == 'exploits')]
 del exploits['attack_cat']
-fuzzers = df_train[(df_train['attack_cat'] == 'fuzzers')]
+fuzzers = num_trainset[(num_trainset['attack_cat'] == 'fuzzers')]
 del fuzzers['attack_cat']
-dos = df_train[(df_train['attack_cat'] == 'dos')]
+dos = num_trainset[(num_trainset['attack_cat'] == 'dos')]
 del dos['attack_cat']
-reconnaissance = df_train[(df_train['attack_cat'] == 'reconnaissance')]
+reconnaissance = num_trainset[(num_trainset['attack_cat'] == 'reconnaissance')]
 del reconnaissance['attack_cat']
-analysis = df_train[(df_train['attack_cat'] == 'analysis')]
+analysis = num_trainset[(num_trainset['attack_cat'] == 'analysis')]
 del analysis['attack_cat']
-backdoor = df_train[(df_train['attack_cat'] == 'backdoor')]
+backdoor = num_trainset[(num_trainset['attack_cat'] == 'backdoor')]
 del backdoor['attack_cat']
-shellcode = df_train[(df_train['attack_cat'] == 'shellcode')]
+shellcode = num_trainset[(num_trainset['attack_cat'] == 'shellcode')]
 del shellcode['attack_cat']
-worms = df_train[(df_train['attack_cat'] == 'worms')]
+worms = num_trainset[(num_trainset['attack_cat'] == 'worms')]
 del worms['attack_cat']
+normal = num_trainset[(num_trainset['attack_cat'] == 'normal')]
+del normal['attack_cat']
 
 # drop 'attack_cat' for now
-del df_train['attack_cat']
+del num_trainset['attack_cat']
 
+# Now we have 9 different data-frames, each one referred to a specific attack category, all numerical values
+# no 'attack_cat' feature. We still have df_train as the original dataset.
 
 # for layer1, PCC between:
 # generic - rest
@@ -141,56 +155,28 @@ del df_train['attack_cat']
 # dos - rest
 # select common features for all of these attacks
 
-# Select the correlated features in the two datasets.
-def get_most_correlated_features(x1, x2, n=10):
-    # Convert all datasets to numbers
-    x1 = x1.apply(pd.to_numeric, errors='ignore')
-    x2 = x2.apply(pd.to_numeric, errors='ignore')
-
-    # Calculate the correlation matrix between the two DataFrames.
-    corr_matrix = x1.corrwith(x2)
-
-    # Calculate the correlation matrix between the two DataFrames.
-    corr_matrix = x1.corrwith(x2)
-
-    # Select the features that are most correlated between the two DataFrames.
-    most_correlated_features = corr_matrix.abs().sort_values(ascending=False).index[:n]
-
-    # Return the most important features.
-    return most_correlated_features
-
-
-# compute all elements of df1 that are not in df2
-def compute_set_difference(df1, df2):
-    # Create a new DataFrame containing the set difference of the two DataFrames.
-    df_diff = df1[~df1.index.isin(df2.index)]
-
-    # Return the DataFrame.
-    return df_diff
-
-
 # start with l1
-rest = compute_set_difference(df_train, generic)
+rest = compute_set_difference(num_trainset, generic)
 generic_all = get_most_correlated_features(rest, generic)
-print('Generic: ', df_train.shape, generic.shape)
+print('Generic: ', num_trainset.shape, generic.shape)
 print('Correlated features between Generic attacks and all data: ', len(generic_all))
 print(generic_all)
 
-rest = compute_set_difference(df_train, exploits)
+rest = compute_set_difference(num_trainset, exploits)
 exploits_all = get_most_correlated_features(rest, exploits)
-print('Exploits: ', df_train.shape, exploits.shape)
+print('Exploits: ', num_trainset.shape, exploits.shape)
 print('Correlated features between Exploit attacks and all data: ', len(exploits_all))
 print(exploits_all)
 
-rest = compute_set_difference(df_train, fuzzers)
+rest = compute_set_difference(num_trainset, fuzzers)
 fuzzers_all = get_most_correlated_features(rest, fuzzers)
-print('Fuzzers: ', df_train.shape, fuzzers.shape)
+print('Fuzzers: ', num_trainset.shape, fuzzers.shape)
 print('Correlated features between Fuzzers attacks and all data: ', len(fuzzers_all))
 print(fuzzers_all)
 
-rest = compute_set_difference(df_train, dos)
+rest = compute_set_difference(num_trainset, dos)
 dos_all = get_most_correlated_features(rest, dos)
-print('DoS: ', df_train.shape, dos.shape)
+print('DoS: ', num_trainset.shape, dos.shape)
 print('Correlated features between Generic attacks and all data: ', len(dos_all))
 print(dos_all)
 
@@ -203,34 +189,31 @@ set_generic = set(generic_all)
 common_features_l1 = set_generic & set_exploits & set_fuzzers & set_dos
 print('Common features to train l1: ', len(common_features_l1), common_features_l1)
 
-# now for l2
-rest = compute_set_difference(df_train, reconnaissance)
-reconnaissance_all = get_most_correlated_features(rest, reconnaissance)
-print('Reconnaissance: ', df_train.shape, reconnaissance.shape)
+# now l2 needs the features to describe the difference between rare attacks and normal traffic
+normal = normal[numerical_features]
+
+reconnaissance_all = get_most_correlated_features(normal, reconnaissance)
+print('Reconnaissance: ', normal.shape, reconnaissance.shape)
 print('Correlated features between reconnaissance attacks and all data: ', len(reconnaissance_all))
 print(reconnaissance_all)
 
-rest = compute_set_difference(df_train, analysis)
-analysis_all = get_most_correlated_features(rest, analysis)
-print('analysis: ', df_train.shape, analysis.shape)
+analysis_all = get_most_correlated_features(normal, analysis)
+print('analysis: ', normal.shape, analysis.shape)
 print('Correlated features between analysis attacks and all data: ', len(analysis_all))
 print(analysis_all)
 
-rest = compute_set_difference(df_train, backdoor)
-backdoor_all = get_most_correlated_features(rest, backdoor)
-print('backdoor: ', df_train.shape, backdoor.shape)
+backdoor_all = get_most_correlated_features(normal, backdoor)
+print('backdoor: ', normal.shape, backdoor.shape)
 print('Correlated features between backdoor attacks and all data: ', len(backdoor_all))
 print(backdoor_all)
 
-rest = compute_set_difference(df_train, shellcode)
-shellcode_all = get_most_correlated_features(rest, shellcode)
-print('shellcode: ', df_train.shape, shellcode.shape)
+shellcode_all = get_most_correlated_features(normal, shellcode)
+print('shellcode: ', normal.shape, shellcode.shape)
 print('Correlated features between shellcode attacks and all data: ', len(shellcode_all))
 print(shellcode_all)
 
-rest = compute_set_difference(df_train, worms)
-worms_all = get_most_correlated_features(rest, worms)
-print('worms: ', df_train.shape, worms.shape)
+worms_all = get_most_correlated_features(normal, worms)
+print('worms: ', normal.shape, worms.shape)
 print('Correlated features between worms attacks and all data: ', len(worms_all))
 print(worms_all)
 
@@ -243,3 +226,49 @@ set_reconnaissance = set(reconnaissance_all)
 
 common_features_l2 = set_worms & set_shellcode & set_backdoor & set_analysis & set_reconnaissance
 print('Common features to train l2: ', len(common_features_l2), common_features_l2)
+
+# two different train sets for l1 and l2. Each one must contain the samples from the original dataset according
+# to the division in layers, and the features obtained from the ICFS as the only features.
+x_train_l1 = df_train[(df_train['attack_cat'] == 'generic') | (df_train['attack_cat'] == 'exploits')
+                     | (df_train['attack_cat'] == 'fuzzers') | (df_train['attack_cat'] == 'dos')]
+
+x_train_l1 = x_train_l1[list(common_features_l1)]
+
+x_train_l2 = df_train[(df_train['attack_cat'] == 'worms') | (df_train['attack_cat'] == 'shellcode')
+                     | (df_train['attack_cat'] == 'reconnaissance') | (df_train['attack_cat'] == 'backdoor')
+                    | (df_train['attack_cat'] == 'analysis')]
+
+x_train_l2 = x_train_l2[list(common_features_l2)]
+
+'''
+# MinMax scales the values of df_train between 0-1
+scaler1 = MinMaxScaler()
+
+# scaling the numerical values of the train set
+df_minmax = scaler1.fit_transform(x_train_l1[numerical_features])
+x_train_l1[numerical_features] = pd.DataFrame(df_minmax, columns=x_train_l1[numerical_features].columns)
+x_train_l2[numerical_features] = pd.DataFrame(df_minmax, columns=x_train_l2[numerical_features].columns)
+
+# Perform One-hot encoding of the categorical values
+ohe = OneHotEncoder(handle_unknown='ignore')
+label_enc = ohe.fit_transform(x_train_l1[features_to_encode])
+label_enc.toarray()
+new_labels = ohe.get_feature_names_out(features_to_encode)
+df_enc = pd.DataFrame(data=label_enc.toarray(), columns=new_labels)
+x_train_l1 = pd.concat([x_train_l1[numerical_features], df_enc],
+                     axis=1)  # df_train includes the newly one-hot-encoded columns
+
+label_enc = ohe.fit_transform(x_train_l2[features_to_encode])
+label_enc.toarray()
+new_labels = ohe.get_feature_names_out(features_to_encode)
+df_enc = pd.DataFrame(data=label_enc.toarray(), columns=new_labels)
+x_train_l2 = pd.concat([x_train_l2[numerical_features], df_enc],
+                     axis=1)  # df_train includes the newly one-hot-encoded columns
+
+# sort the columns
+x_train_l1 = x_train_l1.sort_index(axis=1)
+x_train_l2 = x_train_l2.sort_index(axis=1)
+
+print(x_train_l1)
+print(x_train_l2)
+'''
